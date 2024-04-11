c2efbf708d4cc80ee9665fa795c79cf810da938a
lois181 <91326253+lois181@users.noreply.github.com>
2024-04-11 13:34:24 +0100

    Update project_c5_c5.py
    fixing bugs related to signac post conditions

--- my_project/project_c5_c5.py
+++ my_project/project_c5_c5.py
@@ -22,9 +22,9 @@ nvt_mdp_file = "md.mdp"
 PI_grofile = "PI.gro"
 c5_grofile = "plasticizer.gro"
 
-c_sim = MyProject.make_group(name="c5_c5_sim") 
+c5_c5_sim = MyProject.make_group(name="c5_c5_sim") 
 
-def condition_c_c5(job): # defining my condition for C_C5 simulation
+def condition_c5_c5(job): # defining my condition for C5_C5 simulation
     if job.statepoint().get("sim_type") == "C5_C5": 
         return True 
         
@@ -45,7 +45,6 @@ def make_box_c5_c5(c5_num):
 def mdrun_min_c5_c5():
     ''' helper function to perform the long minimisation '''
     cmd = ("""sed -i 's/0.6204/1.1  /' ff-PI5.itp
-    sed -i 's/0.6204/1.1  /' ff-PI5.itp
     gmx_mpi grompp -f minim.mdp -c out.gro -n index.ndx -p topol.top -o em.tpr
     gmx_mpi mdrun -s em.tpr -tableb table_a1.xvg
     sed -i 's/1.1 /1.0 /' ff-PI5.itp
@@ -63,14 +62,15 @@ def mdrun_min_c5_c5():
     sed -i 's/0.7 /0.6204/' ff-PI5.itp
     gmx_mpi grompp -f minim.mdp -c confout.gro -n index.ndx -p topol.top -o em.tpr
     gmx_mpi mdrun -s em.tpr -tableb table_a1.xvg
-    mv confout.gro confout_min.gro""")
+    mv confout.gro confout_min.gro
+""")
     
     return cmd 
 
 def grompp_npt(): 
     ''' helper function for npt grompp '''
     cmd = (
-        "{gmx} grompp -c {gro_file} -f {npt_mdp} -n {index} -p {topol} -o topol.tpr"
+        "{gmx} grompp -c {gro_file} -f {npt_mdp} -n {index} -p {topol} -o topol-npt.tpr"
         .format(
             gmx=gmx_mpi,
             gro_file=gro_file_min,
@@ -106,24 +106,32 @@ def mdrun():
         )
     return cmd
 
-
+def mdrun_npt():
+    ''' helper function for generic mdrun '''
+    cmd = (
+        """{gmx} mdrun -s topol-npt.tpr -tableb table_a1.xvg
+        mv confout.gro confoutNPT.gro"""
+        .format(
+            gmx=gmx_mpi)
+        )
+    return cmd
 
 @c5_c5_sim
-@MyProject.pre(condition_c5_c5)
 @MyProject.post.isfile("out.gro")
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+@MyProject.pre(condition_c5_c5)
+@MyProject.operation(with_job=True, cmd=True)
 def box_c5_c5(job):
     with job:
         flex = job.statepoint().get("flexibility")
         if flex == "r":
             PL_conc = job.statepoint().get("PL_conc")
-            back_len = job.statepoint().get("backbone_length")
+            back_len = job.statepoint().get("backbone_length") - 2
             side_len = job.statepoint().get("side_chain_length")
             freq = job.statepoint().get("side_chain_frequency")
             if side_len == 0:
-                PL_size = back_len
+                PL_size = back_len + 2
             else:
-                PL_size = back_len + math.ceil(back_len/freq)*side_len
+                PL_size = back_len + math.ceil(back_len/freq)*side_len + 2
 
         if flex == "f":
             PL_conc = job.statepoint().get("PL_conc")
@@ -135,24 +143,33 @@ def box_c5_c5(job):
             else:
                 PL_size = back_len + math.ceil(back_len/freq)*side_len  + 2
 
-        c5_num = round(((PL_conc/100) * 72 * 300)/ PL_size)
+        c5_num = round(6500/PL_size)
 
         return make_box_c5_c5(c5_num)
    
 
 @c5_c5_sim
 @MyProject.pre(condition_c5_c5)
-@MyProject.pre.after(box_c5_c5)
-@MyProject.post.isfile("confout_min.gro") 
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+@MyProject.post.isfile("confout_min.gro")
+@MyProject.operation(with_job=True, cmd=True)
 def do_min(job):
     with job:
-        return mdrun_min()
+        return mdrun_min_c5_c5()
 
+
+#@c5_c5_sim
+#@MyProject.pre(condition_c5_c5)
+#@MyProject.post.isfile("confout_min.gro")
+#@MyProject.operation(with_job=True, cmd=True)
+#def move_confout1(job):
+#    with job:
+#        cmd = "mv confout.gro confout_min.gro"
+#        return cmd
     
 @c5_c5_sim       
 @MyProject.pre(condition_c5_c5)
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+@MyProject.post.isfile("topol-npt.tpr")
+@MyProject.operation(with_job=True, cmd=True)
 def perform_grompp_npt(job):
     with job:
         return grompp_npt()
@@ -160,56 +177,76 @@ def perform_grompp_npt(job):
 
 @c5_c5_sim       
 @MyProject.pre(condition_c5_c5)
-@MyProject.pre.after(perform_grompp_npt)
+#@MyProject.pre.after(perform_grompp_npt)
 @MyProject.post.isfile("confoutNPT.gro")
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+@MyProject.operation(with_job=True, cmd=True)
 def perform_mdrun_npt(job):
     with job:
-        return mdrun()
+        return mdrun_npt()
     
-@c5_c5_sim
-@MyProject.pre(condition_c5_c5)
-@MyProject.pre.after(perform_mdrun_npt)
-@MyProject.post.isfile("confoutNPT.gro")
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
-def move_confout(job):
-    with job:
-        cmd = "mv confout.gro confoutNPT.gro"
-        return cmd
+#@c5_c5_sim
+#@MyProject.pre(condition_c5_c5)
+#@MyProject.pre.isfile("confout.gro")
+#@MyProject.post.isfile("confoutNPT.gro")
+#@MyProject.operation(with_job=True, cmd=True)
+#def move_confout(job):
+#    with job:
+#        cmd = "mv confout.gro confoutNPT.gro"
+#        return cmd
 
 @c5_c5_sim  
 @MyProject.pre(condition_c5_c5)
-@MyProject.pre.after(move_confout)
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+#@MyProject.pre.after(move_confout)
+@MyProject.post.isfile("topol.tpr")
+@MyProject.operation(with_job=True, cmd=True)
 def perform_grompp_nvt(job):
     with job:
         return grompp_nvt()
 
+
 @c5_c5_sim
 @MyProject.pre(condition_c5_c5)
-@MyProject.pre.after(perform_grompp_nvt)
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+#@MyProject.pre.after(perform_grompp_nvt)
+@MyProject.post.isfile("confout.gro")
+@MyProject.operation(with_job=True, cmd=True)
 def perform_mdrun_nvt(job):
     with job:
         return mdrun()
 
 
 @c5_c5_sim
+@MyProject.pre(condition_c5_c5)
 @MyProject.pre.isfile("traj.trr")
-@MyProject.pre.isfile("\#*")
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+#@MyProject.pre.isfile("\#*")
+@MyProject.operation(with_job=True, cmd=True)
 def clean(job):
     with job:
         """ removing the repeat files and also the .trr file which you don't need for any analysis """
         cmd = (
-            """rm \#*
+            """
             rm traj.trr """
             )
         return cmd
 
+
 @c5_c5_sim
-@MyProject.pre.after(clean)
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+@MyProject.pre(condition_c5_c5)
+@MyProject.pre.isfile("#md.log.1#")
+#@MyProject.pre.isfile("\#*")
+@MyProject.operation(with_job=True, cmd=True)
+def clean2(job):
+    with job:
+        """ removing the repeat files and also the .trr file which you don't need for any analysis """
+        cmd = (
+            """rm \#* """
+            )
+        return cmd
+
+@c5_c5_sim
+@MyProject.pre(condition_c5_c5)
+#@MyProject.pre.after(clean)
+@MyProject.post.isfile("poly1.txt")
+@MyProject.operation(with_job=True, cmd=True)
 def get_ent_files(job):
     """ doing gmx rdf to calculate it at each time """
     with job:
@@ -217,13 +254,13 @@ def get_ent_files(job):
         flex = job.statepoint().get("flexibility")
         if flex == "r":
             PL_conc = job.statepoint().get("PL_conc")
-            back_len = job.statepoint().get("backbone_length")
+            back_len = job.statepoint().get("backbone_length") - 2
             side_len = job.statepoint().get("side_chain_length")
             freq = job.statepoint().get("side_chain_frequency")
             if side_len == 0:
-                PL_size = back_len
+                PL_size = back_len + 2
             else:
-                PL_size = back_len + math.ceil(back_len/freq)*side_len
+                PL_size = back_len + math.ceil(back_len/freq)*side_len + 2
     
         if flex == "f":
             PL_conc = job.statepoint().get("PL_conc")
@@ -235,26 +272,27 @@ def get_ent_files(job):
             else:
                 PL_size = back_len + math.ceil(back_len/freq)*side_len  + 2
     
-        c5_num = round(((PL_conc/100) * 72 * 300)/ PL_size)
+        c5_num = round(6500/PL_size)
 
         cmd = (
         """for i in `seq 1 {num}`
     do
     
-    echo "$i" | gmx_mpi polystat -f traj_comp.xtc -n index_ent.ndx -s topol.tpr -o poly"$i".xvg -b 500000
+    echo "$i" | gmx_mpi polystat -f traj_comp.xtc -n index_ent.ndx -s topol.tpr -o poly"$i".xvg
     
     
     done
      """.format(
-                num = C5_num,
+                num = c5_num,
             )
         )         
         return cmd
     
 
 @c5_c5_sim
-@MyProject.pre.after(get_ent_files)
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
+@MyProject.pre(condition_c5_c5)
+#@MyProject.pre.after(get_ent_files)
+@MyProject.operation(with_job=True)
 def perform_analysis(job):
     with job:
         ext = ('.xvg')
@@ -264,9 +302,10 @@ def perform_analysis(job):
         no = '#'
         no1 = '@'
         no2 = '&'
-        start = 1000000 # time to start at
-        space = 600000 # time between tests
+        start = 200 # time to start at
+        space = 100 # time between tests
         stop = job.statepoint().get("sim_time")
+        stop = 600
         length = np.arange(start, stop, space)
         length2 = np.arange(start-space, stop-space, space)
         
@@ -335,23 +374,35 @@ def perform_analysis(job):
         
         job.doc["entropy_PLPL"] = np.average(entropies)  
             
-
+        return None 
 
     
     
 @c5_c5_sim
-@MyProject.pre.isfile("poly*")
-@MyProject.pre.isfile("\#*")
-@MyProject.pre.after(perform_analysis)
-@MyProject.operation(with_job=True, cmd=True, directives={"np": 40})
-def clean2(job):
+@MyProject.pre(condition_c5_c5)
+@MyProject.pre.isfile("poly1.xvg")
+#@MyProject.pre.after(perform_analysis)
+@MyProject.operation(with_job=True, cmd=True)
+def clean3(job):
     """ removing the poly files because there are too many """
     with job:
         cmd = (
-            """rm poly*
-            rm \#* """
+            "rm poly*"
             )
         return cmd
+
+@c5_c5_sim
+@MyProject.pre(condition_c5_c5)
+@MyProject.pre.isfile("#poly1.xvg.1#")
+@MyProject.operation(with_job=True, cmd=True)
+def clean4(job):
+    """ removing the poly files because there are too many """
+    with job:
+        cmd = (
+            "rm \#* "
+            )
+        return cmd
+
 
 if __name__ == "__main__":
     MyProject().main()
